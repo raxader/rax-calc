ff3cc72af1238ed8561f8c0817a2af7af728fcee
rax <42217808+raxader@users.noreply.github.com>
2021-01-23 02:23:11 +0000

    Update calc.go

--- calc.go
+++ calc.go
@@ -1,96 +1,75 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	var choice int
-	fmt.Print("[1] Addition\n[2] Subtraction\n[3] Multiplication\n[4] Division\n\nWhat would you like to do?\n> ")
+
+	fmt.Print("[1] Addition\n[2] Subtraction\n[3] Multiplication\n[4] Division\n\n[5] Square Root\n\nWhat would you like to do?\n> ")
 	fmt.Scanln(&choice)
 
+	//create two variables two perform math on
+	var numberOne float64
+	var numberTwo float64
+
+	//bypass for sqrt only needing one argument
+	if choice == 5 {
+		sqrt()
+		return
+	}
+
+	//get user input to store into the two variables
+	fmt.Print("First number:\n> ")
+	fmt.Scanln(&numberOne)
+	fmt.Print("Second number:\n> ")
+	fmt.Scanln(&numberTwo)
+	fmt.Print("\n") // a dumb solution.
+
+	//call the chosen function and insert the two variables into it
 	switch choice {
 	case 1:
-		add()
+		add(numberOne, numberTwo)
 	case 2:
-		subt()
+		subt(numberOne, numberTwo)
 	case 3:
-		mult()
+		mult(numberOne, numberTwo)
 	case 4:
-		div()
+		div(numberOne, numberTwo)
 	default:
 		fmt.Print("\nNot an option!!\n\n") // you may notice i enjoy newlines quite a bit.
 		main()
 	}
 }
 
-func rerun() {
-	var choice string
-	fmt.Println("Is there anything else you would like to do? [y/n]")
-	fmt.Scanln(&choice)
-
-	switch choice {
-	case "y":
-		main()
-	case "n":
-		fmt.Println("Bye")
-	default:
-		fmt.Println("Not an option!!")
-		rerun()
-	}
-
+//function with two arguments to slot things into
+func add(add1 float64, add2 float64) {
+	//add the two arguments which now have numberOne and numberTwo stored in them
+	addSum := add1 + add2
+	fmt.Println(addSum)
 }
 
-func add() {
-	var addOne float64
-	var addTwo float64
-
-	fmt.Print("First number:\n> ")
-	fmt.Scanln(&addOne)
-	fmt.Print("Second number:\n> ")
-	fmt.Scanln(&addTwo)
-	fmt.Print("\n") // a dumb solution.
-
-	fmt.Println(addOne + addTwo)
-	rerun()
+func subt(subt1 float64, subt2 float64) {
+	subtSum := subt1 - subt2
+	fmt.Println(subtSum)
 }
 
-func subt() {
-	var subtOne float64
-	var subtTwo float64
-
-	fmt.Print("First number:\n> ")
-	fmt.Scanln(&subtOne)
-	fmt.Print("Second number:\n> ")
-	fmt.Scanln(&subtTwo)
-	fmt.Print("\n")
-
-	fmt.Println(subtOne - subtTwo)
-	rerun()
+func mult(mult1 float64, mult2 float64) {
+	multSum := mult1 * mult2
+	fmt.Println(multSum)
 }
 
-func mult() {
-	var multOne float64
-	var multTwo float64
-
-	fmt.Print("First number:\n> ")
-	fmt.Scanln(&multOne)
-	fmt.Print("Second number:\n> ")
-	fmt.Scanln(&multTwo)
-	fmt.Print("\n")
-
-	fmt.Println(multOne * multTwo)
-	rerun()
+func div(div1 float64, div2 float64) {
+	divSum := div1 / div2
+	fmt.Println(divSum)
 }
 
-func div() {
-	var divOne float64
-	var divTwo float64
+func sqrt() {
+	var sqrtOne float64
+	fmt.Print("Number:\n> ")
+	fmt.Scanln(&sqrtOne)
 
-	fmt.Print("First number:\n> ")
-	fmt.Scanln(&divOne)
-	fmt.Print("Second number:\n> ")
-	fmt.Scanln(&divTwo)
-	fmt.Print("\n")
-
-	fmt.Println(divOne / divTwo)
-	rerun()
+	fmt.Println(math.Sqrt(sqrtOne))
 }
